f3c4a21bcafe0b3f62f6c4fab2b9aa53a24c882a
Alam <alam.15@outlook.com>
2021-11-03 22:54:47 -0400

    jobs page layout

---
+++ pages/companies/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const CompaniesPage: FC = function () {
+  return <h1>Companies</h1>;
+};
+
+export default CompaniesPage;
---
+++ pages/jobs/index.tsx
@@ -0,0 +1,61 @@
+import { FC, Fragment } from "react";
+
+import Recents from "../../components/PopularSearches/Recents";
+
+const Jobs: FC = function () {
+  return (
+    <Fragment>
+      <header>
+        <div className="container">
+          <div className="h-48 flex items-center">
+            <form className="w-full flex justify-center">
+              <div className="inline-block rounded-md border border-gray-400 flex py-2 px-4 mr-3  w-1/4">
+                <label htmlFor="job-keyword" className="inline-block pr-2">
+                  What
+                </label>
+                <input
+                  type="search"
+                  id="job-keyword"
+                  placeholder="Title, keyword or company"
+                  className="w-full focus:outline-none"
+                />
+              </div>
+              <div className="rounded-md border border-gray-400 flex py-2 px-4 focus:border-black w-1/4">
+                <label htmlFor="province" className="inline-block pr-2">
+                  Where
+                </label>
+                <input
+                  type="text"
+                  id="province"
+                  placeholder="Province"
+                  className="w-full focus:outline-none"
+                />
+              </div>
+              <button
+                type="submit"
+                className="rounded-md bg-black text-white px-5 py-1 ml-4 uppercase font-bold"
+              >
+                Find
+              </button>
+            </form>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className="container">
+          <div className="flex">
+            <section className="w-9/12 float-left">
+              <h2 className="text-4xl font-bold">News </h2>
+            </section>
+            <aside className="w-3/12 float-right">
+              <h3 className="text-2xl font-bold">Recent searches</h3>
+              <Recents />
+            </aside>
+          </div>
+        </div>
+      </main>
+    </Fragment>
+  );
+};
+
+export default Jobs;
