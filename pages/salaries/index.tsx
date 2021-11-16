55c57f992d08524e8b77a765b925751a1a8cf371
Alam <alam.15@outlook.com>
2021-11-16 14:32:48 -0400

    salaries page

---
+++ pages/salaries/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+import Layout from "../../components/UI/Layout";
+
+const Salaries: FC = function () {
+  return (
+    <Layout>
+      <h1>Salaries</h1>
+    </Layout>
+  );
+};
+
+export default Salaries;
